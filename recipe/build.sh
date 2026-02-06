2cdc5b353a1fbf80d3c42c1a96d46d8e10c723bb
Daniel Ching <9604511+carterbox@users.noreply.github.com>
2026-02-06 14:15:15 -0600

    Check correct directory for glibc-compat

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ set -ex
 [[ -d lib64 ]] && mv lib64 lib
 mkdir -p ${PREFIX}/cuda-compat
 
-check-glibc lib/*.so.*
+check-glibc compat/*.so.*
 
 cp -vd compat/libcuda.so* ${PREFIX}/cuda-compat/
 cp -vd compat/libnvidia-nvvm.so* ${PREFIX}/cuda-compat/
